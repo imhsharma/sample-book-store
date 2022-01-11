e1741f69a10460ffd75ad0db020a570328f3610e
harsh <harsh.sharma683@gmail.com>
2022-01-11 17:46:51 +0530

    checkout-01

--- book-store/src/app/components/checkout/checkout.component.ts
+++ book-store/src/app/components/checkout/checkout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { CartItem } from 'src/app/common/cart-item';
 import { CartService } from 'src/app/service/cart.service';
 
@@ -11,13 +12,49 @@ export class CheckoutComponent implements OnInit {
 
   cartItem:CartItem[]=[];
   totalPrice: number = 0;
+  checkoutFormGroup!:FormGroup;
   totalQuantity: number = 0;
 
-  constructor(private _cartService: CartService) { }
+  constructor(private _cartService: CartService,
+              private formBuilder: FormBuilder) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.cartDetails();
-  }
+    this.checkoutFormGroup = this.formBuilder.group({
+          customer: this.formBuilder.group({
+                    firstName: [''],
+                    lastName: [''],
+                    email: ['']
+                  }),
+          shippingAddress: this.formBuilder.group({
+                    street: [''],
+                    city: [''],
+                    state: [''],
+                    country: [''],
+                    zipcode: ['']
+                  }),
+          billingAddress: this.formBuilder.group({
+                    street: [''],
+                    city: [''],
+                    state: [''],
+                    country: [''],
+                    zipcode: ['']
+                  }),
+          creditCard: this.formBuilder.group({
+                    cardType: [''],
+                    nameOnCard: [''],
+                    cardNumber: [''],
+                    cvv: [''],
+                    expirationMonth: [''],
+                    expirationYear: ['']
+                  }),
+                })
+            
+  const startMonth: number = new Date().getMonth() + 1;
+  console.log("startMonth: " + startMonth);
+            
+             
+        }
 
   cartDetails(){
     this.cartItem = this._cartService.cartItems;
@@ -28,5 +65,23 @@ export class CheckoutComponent implements OnInit {
     this._cartService.calculateTotalPrice();
 
   }
+
+  onSubmit() {
+    console.log('Purchase the books');
+    // console.log(this.checkoutFormGroup.get('customer').value);
+    // console.log("Emial is", this.checkoutFormGroup.get('customer').value.email);
+  }
+
+  copyShippingAddressToBillingAddress(event:any) {
+
+    if (event.target.checked) {
+      this.checkoutFormGroup.controls.billingAddress
+        .setValue(this.checkoutFormGroup.controls.shippingAddress.value);
+    }else {
+      this.checkoutFormGroup.controls.billingAddress.reset();
+    }
+    
+  }
+  
 
 }
